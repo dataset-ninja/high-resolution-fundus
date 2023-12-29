2606c6a6ef74728fb485126e34ae2a6922b79a6d
peixed351 <120389559+peixed351@users.noreply.github.com>
2023-12-29 18:03:16 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www5.cs.fau.de/research/data/fundus-images/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 11881605
+PREVIEW_IMAGE_ID: int = 11886395
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/high-resolution-fundus"
